2f32bc10091028d4c72adf39031676f667b62868
Rodolfo Alejandro Aguiar Medina <rodolfo269301@gmail.com>
2025-07-30 22:32:37 -0600

    URL actualizado

--- configuration.py
+++ configuration.py
@@ -1,4 +1,4 @@
-URL_SERVICE = "https://cnt-dadfeada-8e2b-4c00-a78f-3c111ee26bef.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-d495b767-fdd6-4ce6-9e3c-9a49e91d9e7d.containerhub.tripleten-services.com"
 DOC_PATH = "/docs/"
 CREATE_USER_PATH = "/api/v1/users"
 KITS_PATH = "/api/v1/kits"
